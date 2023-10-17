545946261afa53acdc50b00b8a6eac0476362480
Wellington Rodrigues <wellrodrigues@gmail.com>
2023-10-17 09:56:25 -0300

    Criando ms de autenticação

---
+++ ms-wManager-auth/src/main/java/com/wmanager/auth/MsWManagerAuthApplication.java
@@ -0,0 +1,13 @@
+package com.wmanager.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsWManagerAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsWManagerAuthApplication.class, args);
+	}
+
+}
---
+++ ms-wManager-auth/src/test/java/com/wmanager/auth/MsWManagerAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wmanager.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsWManagerAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

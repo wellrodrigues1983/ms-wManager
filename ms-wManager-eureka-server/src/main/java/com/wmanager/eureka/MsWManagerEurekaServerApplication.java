1969747b7b252e8fee60d48e09d20a8fea1716a7
Wellington Rodrigues <wellrodrigues@gmail.com>
2023-11-13 17:37:51 -0300

    Criando e configurando Eureka Server

---
+++ ms-wManager-eureka-server/src/main/java/com/wmanager/eureka/MsWManagerEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.wmanager.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class MsWManagerEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsWManagerEurekaServerApplication.class, args);
+	}
+
+}
---
+++ ms-wManager-eureka-server/src/test/java/com/wmanager/eureka/MsWManagerEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wmanager.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsWManagerEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
